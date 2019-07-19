606cdaecf67195a08d8ee44e21881bbe395aed23
J. Abbott <abbotto@users.noreply.github.com>
2019-07-19 18:27:23 -0230

    feat: support aws iam roles

--- aws-mfa-session.sh
+++ aws-mfa-session.sh
@@ -6,17 +6,22 @@
 
 # Imported
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-}
-AWS_MFA_DEVICE_ARN=${AWS_MFA_DEVICE_ARN:-}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-}
 AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN:-}
 
 # Local
+aws_iam_user_device_arn=$(aws sts get-caller-identity --output text --query 'Arn' | sed -r 's|:user|:mfa|g')
+
 aws_session_path="${HOME}"/.aws
 mkdir -p "${aws_session_path}"
 
-aws_session_credentials=$(touch "${aws_session_path}"/session-credentials; cat "${aws_session_path}"/session-credentials)
+aws_iam_role_session_credentials=$(\
+    touch "${aws_session_path}"/session-credentials;\
+    cat "${aws_session_path}"/session-credentials\
+)
+
 aws_session_duration=86400
-aws_session_expiry_date="$(echo "${aws_session_credentials}" | awk '{ print $3 }')"
+aws_session_expiry_date="$(echo "${aws_iam_role_session_credentials}" | awk '{ print $3 }')"
 
 ################################################################
 # Fetch and cache AWS session credentials
@@ -62,16 +67,36 @@ else
     fi
 
     # Use the provided credentials to generate session credentials
-    printf '%s\n' "    Acquiring AWS session credentials: \"aws sts get-session-token --serial-number ${AWS_MFA_DEVICE_ARN} --token-code ${aws_mfa_security_code}\"";
-    aws_session_credentials=$(\
+    printf '%s\n' "    Acquiring AWS session credentials: \"aws sts get-session-token --serial-number ${aws_iam_user_device_arn} --token-code ${aws_mfa_security_code}\"";
+    aws_iam_user_session_credentials=$(\
         aws sts get-session-token \
         --duration "${aws_session_duration}" \
-        --serial-number "${AWS_MFA_DEVICE_ARN}" \
-        --token-code "${aws_mfa_security_code}"\
+        --serial-number "${aws_iam_user_device_arn}" \
+        --token-code "${aws_mfa_security_code}" \
         --output text \
     )
 
-    printf '%s\n' "${aws_session_credentials}" | tee "${aws_session_path}"/session-credentials >/dev/null 2>&1
+    # Set the AWS MFA credential variables
+    AWS_ACCESS_KEY_ID="$(echo "${aws_iam_user_session_credentials}" | awk '{ print $2 }')"
+    AWS_SECRET_ACCESS_KEY="$(echo "${aws_iam_user_session_credentials}" | awk '{ print $4 }')"
+    AWS_SESSION_TOKEN="$(echo "${aws_iam_user_session_credentials}" | awk '{ print $5 }')"
+
+    export AWS_ACCESS_KEY_ID
+    export AWS_SECRET_ACCESS_KEY
+    export AWS_SESSION_TOKEN
+
+    session_id=$(date +%s)
+
+    # Generate role session credentials
+    aws_iam_role_session_credentials=$(\
+        aws sts assume-role \
+        --role-arn "${AWS_IAM_ROLE_DEVICE_ARN}" \
+        --role-session-name "aws_mfa_session_${session_id}" \
+        --output text | \
+        grep "^CREDENTIALS"\
+    )
+
+    printf '%s\n' "${aws_iam_role_session_credentials}" | tee "${aws_session_path}"/session-credentials >/dev/null 2>&1
     
     if [ -s "${aws_session_path}"/session-credentials ]; then
         printf '%s\n' "    The AWS session credentials have been updated and will be valid for 24 hours."
@@ -81,10 +106,10 @@ else
     fi
 fi
 
-# Set the AWS MFA credential variables
-AWS_ACCESS_KEY_ID="$(echo "${aws_session_credentials}" | awk '{ print $2 }')"
-AWS_SECRET_ACCESS_KEY="$(echo "${aws_session_credentials}" | awk '{ print $4 }')"
-AWS_SESSION_TOKEN="$(echo "${aws_session_credentials}" | awk '{ print $5 }')"
+# Set the AWS role credential variables
+AWS_ACCESS_KEY_ID="$(echo "${aws_iam_role_session_credentials}" | awk '{ print $2 }')"
+AWS_SECRET_ACCESS_KEY="$(echo "${aws_iam_role_session_credentials}" | awk '{ print $4 }')"
+AWS_SESSION_TOKEN="$(echo "${aws_iam_role_session_credentials}" | awk '{ print $5 }')"
 
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
